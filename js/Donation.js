d75f6b36f32c7feb755e74047ebc286bdce4b2cf
sthetically <s.thetically3@gmail.com>
2025-05-06 18:40:14 -0400

    Create Donation.js

---
+++ js/Donation.js
@@ -0,0 +1,12 @@
+// models/Donation.js
+const mongoose = require("mongoose");
+
+const donationSchema = new mongoose.Schema({
+  amount: { type: Number, required: true },
+  date: { type: Date, default: Date.now },
+  user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  cause: { type: String, required: true },
+});
+
+const Donation = mongoose.model("Donation", donationSchema);
+module.exports = Donation;
