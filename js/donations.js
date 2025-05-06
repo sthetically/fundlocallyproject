fd16c2da7e30e4096b60efaeabb3d587b0bf8ba6
sthetically <s.thetically3@gmail.com>
2025-05-06 18:41:06 -0400

    Create donations.js

---
+++ js/donations.js
@@ -0,0 +1,27 @@
+// routes/donations.js
+const express = require("express");
+const Donation = require("../models/Donation");
+const User = require("../models/User");
+
+const router = express.Router();
+
+// Create a new donation
+router.post("/donate", async (req, res) => {
+  const { userId, amount, cause } = req.body;
+
+  try {
+    const newDonation = new Donation({ user: userId, amount, cause });
+    await newDonation.save();
+
+    // Update user's donation history
+    const user = await User.findById(userId);
+    user.donationHistory.push(newDonation._id);
+    await user.save();
+
+    res.status(201).json({ donation: newDonation });
+  } catch (err) {
+    res.status(500).json({ message: "Server error" });
+  }
+});
+
+module.exports = router;
